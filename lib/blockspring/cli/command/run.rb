8ff7d0c87b14a39a180d875f9338e32a880c3674
Jason Tokoph <jason@tokoph.net>
2014-10-24 17:48:23 -0700

    Stub out run remote and add help

--- lib/blockspring/cli/command/run.rb
+++ lib/blockspring/cli/command/run.rb
@@ -1,8 +1,19 @@
 require "blockspring/cli/command/base"
 
-# set BLOCKSPRING_API_KEY environment variable and run command
+# set BLOCKSPRING_API_KEY environment variable and run block
 #
 class Blockspring::CLI::Command::Run < Blockspring::CLI::Command::Base
+  # run:local COMMAND
+  #
+  # set api key environment variable and run block
+  #
+  # Automatically detects if local or remote
+  # Use run:local or run:remote to specify
+  #
+  #Example:
+  #
+  # $ blockspring run:local node block.js --zip-code=94110
+  #
   def index
     if ENV['BLOCKSPRING_API_KEY'].to_s.strip.empty?
       _, key = Blockspring::CLI::Auth.get_credentials
@@ -11,7 +22,38 @@ class Blockspring::CLI::Command::Run < Blockspring::CLI::Command::Base
       end
     end
 
-    # now run
+    match = /([^\/]+\/)([^\/]+)/.match(@args[0])
+
+    if(match and not File.exist?(@args[0]))
+      remote
+    else
+      local
+    end
+  end
+
+  # run:local COMMAND
+  #
+  # set api key environment variable and run a local block
+  #
+  #Example:
+  #
+  # $ blockspring run:local node block.js --zip-code=94110
+  #
+  def local
     system(*@args)
+  end
+
+  # run:remote USER/BLOCKID
+  #
+  # set api key environment variable and run a remote block
+  #
+  # [--argname=value ...]   # specify arguments for the block
+  #
+  #Example:
+  #
+  # $ blockspring run:remote jtokoph/aasdfj332flk3 --zip-code=94110
+  #
+  def remote
+    error('not yet implimented')
   end
 end
